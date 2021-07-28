1d77efd2ebe8e53c83d82d718283f81e2233325d
Yuuki-F <48887361+Yuuki-F@users.noreply.github.com>
2021-07-29 00:46:15 +0200

    g

--- app.js
+++ app.js
@@ -12,3 +12,30 @@ switcher.addEventListener('click', function() {
     }
 
 });
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
+}
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+async function writeDocument() {
+    while(true) {
+        await sleep(500);
+        document.getElementById('cR').innerHTML = makeid(getRandomInt(55));
+        document.getElementById('cR2').innerHTML = makeid(getRandomInt(55));
+        document.getElementById('cR3').innerHTML = makeid(getRandomInt(55));
+        document.getElementById('cR4').innerHTML = makeid(getRandomInt(55));
+    /*    document.write("aEIOU") */
+    }
+}
+writeDocument();
