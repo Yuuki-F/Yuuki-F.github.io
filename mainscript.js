bc9ba1074c60256b32eb3c98be4b632bf7f04b58
Yuuki-F <48887361+Yuuki-F@users.noreply.github.com>
2022-02-05 01:48:22 +0100

    finalized4tonight

--- mainscript.js
+++ mainscript.js
@@ -1,8 +1,24 @@
-function addVisited(dis)
+/*function addVisited(dis)
 {
+    var elements = document.getElementsByClassName("active");
     if(dis.className=='active') {
         dis.className='';
     }else{
         dis.className='active';
     }
 }
+*/
+function clickFn(evt) {
+    const clickedElement = evt.target;
+  
+    if (!clickedElement.matches('.topnav a')) {
+      return;
+    }
+  
+    document.querySelector(".active").classList.remove("active");
+    clickedElement.classList.add("active");
+  }
+  
+  const topnav = document.querySelector(".topnav");
+  
+  topnav.addEventListener("click", clickFn);
