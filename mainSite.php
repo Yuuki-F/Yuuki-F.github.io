d99a5fbc69f857feb4d706f5d1f4475a15ff4478
Yuuki-F <48887361+Yuuki-F@users.noreply.github.com>
2022-05-27 16:45:27 +0200

    added php

---
+++ mainSite.php
@@ -0,0 +1,6 @@
+<?php
+$Temp=$_POST["temperature"];
+$Humidity=$_POST["humidity"];
+$Write="<p>Temperature: " . $TEMP . " Celsius </p>". "<p>Humidity: ". $Humidity . " % </p>";
+file_put_contents('mainSite.html',$Write);
+?>
