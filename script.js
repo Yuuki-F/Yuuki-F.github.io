09a85f0181d5a7a6faa7297d15b7a73605005427
Yuuki-F <myaccismy@gmail.com>
2023-11-18 00:52:15 +0100

    setup

--- script.js
+++ script.js
@@ -1,30 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add("animate");
-            }
-        });
+$(document).ready(function(){
+    var mouseX, mouseY;
+    var ww = $( window ).width();
+    var wh = $( window ).height();
+    var traX, traY;
+    $(document).mousemove(function(e){
+      mouseX = e.pageX;
+      mouseY = e.pageY;
+      traX = ((4 * mouseX) / 570) + 40;
+      traY = ((4 * mouseY) / 570) + 50;
+      console.log(traX);
+      $(".title").css({"background-position": traX + "%" + traY + "%"});
     });
-
-    document.querySelectorAll("section").forEach(section => {
-        observer.observe(section);
-    });
-});
-
-document.addEventListener('scroll', function() {
-    let currentPos = window.scrollY;
-    let sections = document.querySelectorAll('section');
-    let navLinks = document.querySelectorAll('nav a');
-    
-    sections.forEach(function(section) {
-        if (section.offsetTop <= currentPos && (section.offsetTop + section.offsetHeight) > currentPos) {
-            navLinks.forEach(function(navLink) {
-                navLink.classList.remove('active');
-                if (navLink.getAttribute('href').substring(1) === section.getAttribute('id')) {
-                    navLink.classList.add('active');
-                }
-            });
-        }
-    });
-});
+  });
+  
