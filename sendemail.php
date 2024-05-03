9ff631e285c26b9b89385ecd8e1982a4f842a410
Yuuki-F <myaccismy@gmail.com>
2024-05-03 23:38:06 +0200

    Form actually works now lol

---
+++ sendemail.php
@@ -0,0 +1,41 @@
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = strip_tags(trim($_POST["name"]));
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $message = trim($_POST["message"]);
+
+    if (empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        // Handle error here: Redirect back to form or display a message
+        echo "Oops! There was a problem with your submission. Please complete the form and try again.";
+        exit;
+    }
+
+    // Recipient email (where you want to receive the messages)
+    $recipient = "ceo@splitline.studio";
+
+    // Email subject
+    $subject = "New contact from $name";
+
+    // Email content
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message:\n$message\n";
+
+    // Email headers
+    $email_headers = "From: $name <$email>";
+
+    // Send the email
+    if (mail($recipient, $subject, $email_content, $email_headers)) {
+        // Success message
+        echo "Thank You! Your message has been sent.";
+    } else {
+        // Failure message
+        echo "Oops! Something went wrong and we couldn't send your message.";
+    }
+
+} else {
+    // Not a POST request, set a 403 (forbidden) response code.
+    http_response_code(403);
+    echo "There was a problem with your submission, please try again.";
+}
+?>
